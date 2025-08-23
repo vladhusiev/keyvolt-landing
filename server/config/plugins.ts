f892a15e063762bb943fe3017a256c9f604443cd
Mykhailo <michail.ozdemir@gmail.com>
2025-08-23 22:48:27 +0200

    Change upload provider logic

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,18 +1,38 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'local',
-      providerOptions: {
-        sizeLimit: 1500000, // 1.5MB limit
+export default ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+  if (isProduction) {
+    return {
+      upload: {
+        config: {
+          breakpoints: {
+            xlarge: 1920,
+            large: 1000,
+            medium: 750,
+            small: 500,
+            xsmall: 64,
+            thumbnail: 150,
+          },
+        },
       },
-      breakpoints: {
-        xlarge: 1920,
-        large: 1000,
-        medium: 750,
-        small: 500,
-        xsmall: 64,
-        thumbnail: 150,
+    };
+  }
+
+  return {
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 1500000,
+        },
+        breakpoints: {
+          xlarge: 1920,
+          large: 1000,
+          medium: 750,
+          small: 500,
+          xsmall: 64,
+          thumbnail: 150,
+        },
       },
     },
-  },
-});
+  };
+};
